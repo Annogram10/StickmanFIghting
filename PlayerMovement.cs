54a1af760a479e2b07040f07fc7acef66ba738b4
Annogram10 <vikramsendhil@gmail.com>
2026-04-20 18:26:05 -0500

    Created the initial basic movement for the player. (WASD+space). I am currenlty going to implement the dynamic jumping feature

--- PlayerMovement.cs
+++ PlayerMovement.cs
@@ -2,15 +2,44 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    public float MoveSpeed = 5f;
+    public float jumpForce = 10f;
+    private Rigidbody2D rb;
+    private bool isGrounded;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float moveImput = Input.GetAxis("Horizontal");
+        rb.linearVelocity = new Vector2(moveImput * MoveSpeed, rb.linearVelocity.y);
+
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = true;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = false;
+        }
+
     }
 }
+
+
